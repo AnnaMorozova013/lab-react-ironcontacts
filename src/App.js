542f02f30720168d208116d9c7d6e35556bbe5d9
Anna Morozova <a.morozova.nrw@gmail.com>
2021-12-04 16:49:53 +0100

    iteration 1 done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import allContacts from "./contacts.json";
+import React, { useState } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+  //display 5 first elements
+  const [contacts, setContacts] = useState(allContacts.slice(0, 5))
+
+  return <div className="App">
+    <table>
+      <h1> IronContacts </h1>
+      <tr>
+        <th>Picture</th>
+        <th>Name</th>
+        <th>Popularity</th>
+      </tr>
+
+      {contacts.map(contact => {
+        return (
+
+          <tr>
+            <td><img src={contact.pictureUrl} style={{width: '80px'}} alt=''/></td>
+            <td>{contact.name}</td>
+            <td>{contact.popularity}</td>
+          </tr>
+        )
+      })}
+    </table>
+
+  </div>;
 }
 
 export default App;
